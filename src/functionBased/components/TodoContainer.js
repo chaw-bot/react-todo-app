5eb156e49ac6ec0c41629f3debd843cace192d7d
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-27 01:06:52 +0200

    function based folder

---
+++ src/functionBased/components/TodoContainer.js
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from "react"
+import { v4 as uuidv4 } from "uuid";
+import TodosList from "./TodosList";
+import Header from "./Header";
+import InputTodo from "./InputTodo";
+
+const TodoContainer = () => {
+  const [todos, setTodos] = useState([])
+
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          }
+        }
+        return todo;
+      })
+    );
+  };
+
+  const delTodo = (id) => {
+    setTodos({
+      ...todos.filter((todo) => {
+          return todo.id !== id;
+        })
+    });
+  }
+
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false
+    };
+
+    setTodos([...todos, newTodo]);
+  }
+
+  const setUpdate = (updatedTitle, id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = updatedTitle
+        }
+        return todo
+      })
+    )
+  }
+
+  useEffect(() => {
+    const temp = localStorage.getItem("todos");
+    const loadedTodos = JSON.parse(temp);
+
+    if(loadedTodos) {
+      setTodos(loadedTodos)
+    }
+  }, [setTodos]);
+
+  useEffect(() => {
+    const temp = JSON.stringify(todos);
+    localStorage.setItem("todos", temp)
+  }, [todos]);
+
+  return (
+    <div className="container">
+      <div className="inner">
+        <Header />
+        <InputTodo addTodoProps={addTodoItem} />
+        <TodosList
+          todos={todos}
+          handleChangeProps={handleChange}
+          deleteTodoProps={delTodo}
+          setUpdate={setUpdate}
+        />
+      </div>
+    </div>
+  )
+
+}
+export default TodoContainer
