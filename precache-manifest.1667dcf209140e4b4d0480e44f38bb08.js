af9008a3f66f408ef50ce000a8f3dda46ae0fe10
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-10-27 10:29:50 +0200

    Updates

---
+++ precache-manifest.1667dcf209140e4b4d0480e44f38bb08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4a9160f389d8ddfa5a4da192a99a71d",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "96d84b5ffbe214301881",
+    "url": "/react-todo-app/static/css/main.ee8d8968.chunk.css"
+  },
+  {
+    "revision": "5bc7d0932ceb952a6f21",
+    "url": "/react-todo-app/static/js/2.24f18374.chunk.js"
+  },
+  {
+    "revision": "f7e623707d9b37a3821ec4c3548a4309",
+    "url": "/react-todo-app/static/js/2.24f18374.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96d84b5ffbe214301881",
+    "url": "/react-todo-app/static/js/main.e682212d.chunk.js"
+  },
+  {
+    "revision": "7f607c3e693deaba8e3a",
+    "url": "/react-todo-app/static/js/runtime-main.93dbfc51.js"
+  }
+]);
